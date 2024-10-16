0fd2eb70ca4ce54259f3dfa5981c94f85a51db6a
ManavvBala <manavvbalakrishnan@live.com>
2024-10-16 10:09:00 -0700

    AD5933 testing code

---
+++ main/AD5933.h
@@ -0,0 +1,92 @@
+#ifndef AD5933_H
+#define AD5933_H
+
+#include "driver/i2c_master.h"
+#include "driver/i2c_types.h"
+
+
+/*! AD5933 Address */
+#define AD5933_ADDRESS          ((uint8_t)0x0D)
+
+/*! AD5933_CONTROL_RANGE(x) options */
+#define AD5933_RANGE_2000mVpp   ((uint8_t)0x0)
+#define AD5933_RANGE_200mVpp    ((uint8_t)0x1)
+#define AD5933_RANGE_400mVpp    ((uint8_t)0x2)
+#define AD5933_RANGE_1000mVpp   ((uint8_t)0x3)
+
+/*! AD5933_CONTROL_PGA options */
+#define AD5933_PGA_1        ((uint8_t)0x1)
+#define AD5933_PGA_5        ((uint8_t)0x0)
+
+/*! AD5933 Registers */
+#define AD5933_REG_CONTROL_HB       ((uint8_t)0x80)    /*!< HB of the Control register */
+#define AD5933_REG_CONTROL_LB       ((uint8_t)0x81)    /*!< LB of the Control register */
+#define AD5933_REG_FREQ_START       ((uint8_t)0x82)    /*!< Start frequency */
+#define AD5933_REG_FREQ_INC         ((uint8_t)0x85)    /*!< Frequency increment */
+#define AD5933_REG_INC_NUM          ((uint8_t)0x88)    /*!< Number of increments */
+#define AD5933_REG_SETTLING_CYCLES  ((uint8_t)0x8A)    /*!< Number of settling time cycles */
+#define AD5933_REG_STATUS           ((uint8_t)0x8F)    /*!< Status */
+#define AD5933_REG_TEMP_DATA        ((uint8_t)0x92)    /*!< Temperature data */
+#define AD5933_REG_REAL_DATA        ((uint8_t)0x94)    /*!< Real data MSB*/
+#define AD5933_REG_REAL_DATA_2      ((uint8_t)0x95)    /*!< Real Data LSB*/
+#define AD5933_REG_IMAG_DATA        ((uint8_t)0x96)    /*!< Imaginary data MSB*/
+#define AD5933_REG_IMAG_DATA_2      ((uint8_t)0x97)    /*!<Imaginary data LSB*/
+
+/*! AD5933 Block Commands */
+#define AD5933_BLOCK_WRITE      ((uint8_t)0xA0)
+#define AD5933_BLOCK_READ       ((uint8_t)0xA1)
+#define AD5933_ADDR_POINTER     ((uint8_t)0xB0)
+
+/*! AD5933_CONTROL_FUNCTION(x) options */
+#define AD5933_FUNCTION_INIT_START_FREQ     ((uint8_t)0x1)
+#define AD5933_FUNCTION_START_SWEEP         ((uint8_t)0x2)
+#define AD5933_FUNCTION_INC_FREQ            ((uint8_t)0x3)
+#define AD5933_FUNCTION_REPEAT_FREQ         ((uint8_t)0x4)
+#define AD5933_FUNCTION_MEASURE_TEMP        ((uint8_t)0x9)
+#define AD5933_FUNCTION_POWER_DOWN          ((uint8_t)0xA)
+#define AD5933_FUNCTION_STANDBY             ((uint8_t)0xB)
+
+/*! AD5933_REG_CONTROL_HB Bits */
+#define AD5933_CONTROL_FUNCTION(x)  ((x) << 4)     
+#define AD5933_CONTROL_RANGE(x)     ((x) << 1)
+
+/*! AD5933_REG_STATUS Bits */
+#define AD5933_STAT_TEMP_VALID  (0x1 << 0)
+#define AD5933_STAT_DATA_VALID  (0x1 << 1)
+#define AD5933_STAT_SWEEP_DONE  (0x1 << 2)
+
+#define CLOCK_FREQ 
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!UPDATE THISSS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+extern i2c_device_config_t dev_cfg;
+
+extern i2c_master_dev_handle_t AD5933_dev_handle;
+
+// TO-DO: init device method
+esp_err_t AD5933_init_i2c_device(i2c_master_bus_handle_t bus_handle);
+// i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle);
+
+void AD5933_set_reg_value(uint8_t reg_addr, uint8_t reg_val);
+
+void AD5933_set_ptr_reg(uint8_t reg_addr);
+
+void AD5933_set_reg_block(uint8_t reg_addr, uint8_t* data, uint8_t size);
+
+void AD5933_write_block(uint8_t reg_addr, uint8_t* data, uint8_t size);
+
+// set pointer register to reg_addr before calling this method (allows for repeated calls)
+void AD5933_read_reg(uint8_t* read_buff);
+
+void AD5933_read_reg_block(uint8_t* read_buff, uint8_t num_bytes);
+
+void AD5933_start_freq_sweep(signed short* real_arr, signed short* imag_arr);
+
+// num_incr must fit in 9 bit width!!
+void AD5933_init_settings(double start_freq, double clock_freq, double freq_incr, uint16_t num_incr, uint8_t range, uint8_t PGA, uint16_t num_cycles);
+//TODO:
+//  - calculate gain factor
+//  - calculate gain factor phase
+//  - write arrays to serial monitor
+double AD5933_calculate_impedance(double gainFactor, signed short real, signed short imag);
+
+#endif
---
+++ main/main.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/i2c_master.h"
+#include "driver/i2c_types.h"
+#include "AD5933.h"
+
+#define I2C_MASTER_SCL_IO 9
+#define I2C_MASTER_SDA_IO 8
+
+#define MHz_6 6000000
+#define INTERNAL_CLOCK_FREQ 16000000
+
+i2c_master_bus_config_t i2c_master_config = {
+    .clk_source = I2C_CLK_SRC_DEFAULT,
+    .i2c_port = I2C_NUM_0,
+    .scl_io_num = I2C_MASTER_SCL_IO,
+    .sda_io_num = I2C_MASTER_SDA_IO,
+    .glitch_ignore_cnt = 7,
+    .flags.enable_internal_pullup = true,
+};
+
+int num_incr = 10;
+
+
+// TESTING function for read/write + block read/write
+void AD5933_basic_test() {
+
+    // set + read single register
+    AD5933_set_reg_value(AD5933_REG_FREQ_START, 0x11);
+
+    AD5933_set_ptr_reg(AD5933_REG_FREQ_START);
+
+    uint8_t single_reg = 0;
+    AD5933_read_reg(&single_reg);
+
+    if (single_reg == 0x11) ESP_LOGI("test", "1st test passed");
+    else ESP_LOGI("test", "1st test FAILED!");
+
+    // set + read block register
+    uint8_t test_data[2] = {0x01,0x01};
+    AD5933_write_block(AD5933_REG_INC_NUM, test_data, 2);
+
+    AD5933_set_ptr_reg(AD5933_REG_INC_NUM);
+    uint8_t test_read[2];
+
+    AD5933_read_reg_block(test_read, 2);
+
+    if (test_read[0] == 0x01 && test_read[1] == 0x01) ESP_LOGI("test", "2nd test passed");
+    else ESP_LOGI("test", "2nd test FAILED!");
+}
+
+void print_uarr(uint8_t* arr, uint8_t n) {
+    for (int i = 0; i < n; i++) {
+        ESP_LOGI("arr contents: ", "[%d] = %x", i, arr[i]);
+    }
+}
+
+void print_arr(signed short* arr, uint8_t n) {
+    for (int i = 0; i < n; i++) {
+        ESP_LOGI("arr contents: ", "[%d] = %d", i, arr[i]);
+    }
+}
+
+// TESTING function for more important functionality
+//  - init settings
+//  - start freq sweep
+void app_main(void)
+{
+    ESP_LOGI("log", "starting up");
+    // init master bus
+    i2c_master_bus_handle_t bus_handle;
+
+    //ESP_ERROR_CHECK((&i2c_master_config, bus_handle));
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_master_config, &bus_handle));
+
+    // init AD5933
+
+    AD5933_init_i2c_device(bus_handle);
+
+    // reset
+    AD5933_set_reg_value(AD5933_REG_CONTROL_LB, 0x18);
+
+    
+
+    //AD5933_basic_test();
+
+    AD5933_init_settings(30000, INTERNAL_CLOCK_FREQ, 1000, num_incr, AD5933_RANGE_400mVpp, AD5933_PGA_1, 25);
+    
+    // check to make sure theyre set
+    uint8_t settings[12];
+    AD5933_set_ptr_reg(AD5933_REG_CONTROL_HB);
+    AD5933_read_reg_block(settings, 12);
+    ESP_LOGI("settings", "\n");
+    print_uarr(settings, 12);
+
+    signed short real_arr[num_incr];
+    signed short imag_arr[num_incr];
+
+    AD5933_start_freq_sweep(real_arr, imag_arr);
+
+    print_arr(real_arr, num_incr);
+    print_arr(imag_arr, num_incr);
+
+    for (int i = 0; i < num_incr; i ++){
+        double impedance = AD5933_calculate_impedance(1.0, real_arr[i], imag_arr[i]);
+        ESP_LOGI("impedance", "%f", impedance);
+    }
+
+
+    // logic analyzer test code
+    // while (1) {
+    //     AD5933_set_reg_value(0x80, 0xAA);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+    //AD5933_init_settings(10,10,10,10);
+
+}
+
+// TODO (separate drivers):
+//  - status LEDS
+//  - check for good contact
